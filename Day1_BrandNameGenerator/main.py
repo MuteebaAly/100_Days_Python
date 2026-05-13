b3275b24d9352d3028b263918e674919798d97ca
Muteeba <muteeba131@gmail.com>
2026-05-13 23:12:01 +0500

    Added Day 1 folder and project structure

---
+++ Day1_BrandNameGenerator/main.py
@@ -0,0 +1,13 @@
+# Day 1 - 100 Days of Python Course
+
+print("Welcome to the Brand Name Generator\n:")
+
+city = input("Enter the city that you grew up:\n ")
+
+pet = input("Enter the name of your Pet:\n ")
+
+com = city + pet
+print("You brand name could be:" + com)
+
+print("Thankyou for using this")
+
