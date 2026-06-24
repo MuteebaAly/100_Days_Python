a26d9a0ef12e628174ec2e5c24a4160350429a10
Muteeba <muteeba131@gmail.com>
2026-06-24 14:45:39 +0500

    add hangman word file and add condition if length of guess letter is more than 2 then generate msg

--- Day9_HangmanGame/main.py
+++ Day9_HangmanGame/main.py
@@ -10,92 +10,97 @@ print(r"""
                    |___/          """)
 
 
-words_list = ["pakistan", "blossom", "aesthetic", "gatekeeper", "almirah", "suspicious",'sliper',"poems","multitalennted","hangman","flower"]
+#words_list = ["pakistan", "blossom", "aesthetic", "gatekeeper", "almirah", "suspicious",'sliper',"poems","multitalennted","hangman","flower"] #manually 
+ 
+import hangman_words #import file 
 
-word = random.choice(words_list)
+word = random.choice(hangman_words.word_list)
 dash = []
 
 for i in range(len(word)):
     dash.append('-')
-
 
 life = 6  
 guess = "" 
 
 while life > 0:
     guess = input("Guess a Letter: ").lower()
-    
-    if guess in word:
-        for i in range(len(word)):
-            if word[i] == guess:
-                 dash[i] = word[i]
-        print(dash)
-        
-        if dash != list(word):
-            guess = " "
-        else:
-            print(f"\nGAME OVER !!!\nCONGRATULATIONS YOU WIN 😎👏🎉")
-            break
+
+    if len(guess) > 1:
+        print("Please Enter only Single letter :)")
+    else:
+        if guess in word:
+            for i in range(len(word)):
+                if word[i] == guess:
+                    dash[i] = word[i]
+            print(dash)
             
-    elif guess not in word:
-        life -= 1
-        
-        if life == 5:
-            print(r"""  +----+
+            if dash != list(word):
+                guess = " "
+            else:
+                print(f"\nGAME OVER !!!\nCONGRATULATIONS YOU WIN 😎👏🎉\nYOU GUESS CORRECTLY {word} WORD")
+                break
+                
+        elif guess not in word:
+            life -= 1
+            
+            if life == 5:
+                print(r"""  +----+
   O    |            
        | 
        | 
        | 
        | 
 ====================== """)
-            print(f"you guess {guess} thats not correct. you loose a life \n", dash)
-            
-        elif life == 4:
-            print(r"""  +----+
+                print(f"you guess {guess} thats not correct. you loose a life \n", dash)
+                
+            elif life == 4:
+                print(r"""  +----+
   O    |            
  /     | 
        | 
        | 
        | 
 ====================== """)
-            print(fr"you guess {guess} thats not correct. you loose a life \n", dash)
-            
-        elif life == 3:
-            print(r"""  +----+
+                print(f"you guess {guess} thats not correct. you loose a life \n", dash)
+                
+            elif life == 3:
+                print(r"""  +----+
   O    |            
  /|    | 
        | 
        | 
        | 
 ====================== """)
-            print(f"you guess {guess} thats not correct. you loose a life \n", dash)
+                print(f"you guess {guess} thats not correct. you loose a life \n", dash)
 
-        elif life == 2:
-            print(r"""  +----+
+            elif life == 2:
+                print(r"""  +----+
   O    |            
  /|\   | 
        | 
        | 
        | 
 ====================== """)
-            print(f"you guess {guess} thats not correct. you loose a life \n", dash)
-            
-        elif life == 1: 
-            print(r"""  +----+
+                print(f"you guess {guess} thats not correct. you loose a life \n", dash)
+                
+            elif life == 1: 
+                print(r"""  +----+
   O    |            
  /|\   | 
  /     | 
        | 
        | 
 ====================== """)
-            print(f"you guess {guess} thats not correct. you loose a life \n", dash)
-            
-        else: 
-            print(r"""  +----+
+                print(f"you guess {guess} thats not correct. you loose a life \n", dash)
+                
+            else: 
+                print(r"""  +----+
   O    |            
  /|\   | 
  / \   | 
        | 
        | 
 ====================== """)
-            print(f"GAME OVER !!! 😭\nYou lose all lives. The correct word was '{word}'.")
+                print(f"GAME OVER !!! 😭\nYou lose all lives. The correct word was '{word}'.")
+                break 
